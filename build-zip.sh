bb0e8cd3c1ecf3b3be0697407cef4c42d27485b2
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2020-08-02 01:58:31 +1000

    :green_heart: Building Constellation Cyber * Add Constellation Cyber as a distribution * Update build scripts to support multiple distribututions

--- build-zip.sh
+++ build-zip.sh
@@ -1,56 +1,60 @@
 #!/bin/bash
 set -euo pipefail
 
+if (( $# != 4 ))
+then
+  echo "Invalid arguments parsed."
+  exit 1
+fi
+
 source functions.sh
+
+application=""
+modules=""
+
+while getopts ":a:m:" opt
+do
+  case ${opt} in
+    a )
+      application=$OPTARG
+      ;;
+    m )
+      modules=$OPTARG
+      ;;
+  esac
+done
+
+title "Update Manifest Files With Last Commit Date As Module Version"
+
+for module in $modules
+do
+  cd "../$module"
+  python3 ../constellation-applications/git_version.py -u y
+done
 
 title "Update Dependencies & Clean Build"
 
-cd ../constellation
-ant \
-  -Dnbplatform.active.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
-  -Dbuild.compiler.debug=true update-dependencies-clean-build
+for module in $modules
+do
+  cd "../$module"
+  ant \
+    -Dnbplatform.active.dir="${NETBEANS_HOME}" \
+    -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
+    -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
+    -Dbuild.compiler.debug=true update-dependencies-clean-build
+done
 
-cd ../constellation-adaptors
-ant \
-  -Dnbplatform.active.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
-  -Dbuild.compiler.debug=true update-dependencies-clean-build
+cd "../constellation-applications/$application"
 
-title "Update Manifest Files with new versions"
-
-cd ../constellation
-python3 ../constellation-applications/git_version.py -u y
-cd ../constellation-adaptors
-python3 ../constellation-applications/git_version.py -u y
-
-cd ../constellation-applications/constellation
-
-title "Build Windows Zip"
-
-ant \
-  -Dnbplatform.active.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
-  -Dbuild.compiler.debug=true build-zip-with-windows-jre
-
-title "Build Linux Zip"
-
-ant \
-  -Dnbplatform.active.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
-  -Dbuild.compiler.debug=true build-zip-with-linux-jre
-
-title "Build MacOSX Zip"
-
-ant \
-  -Dnbplatform.active.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
-  -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
-  -Dbuild.compiler.debug=true build-zip-with-macosx-jre
+for target in build-zip-with-windows-jre build-zip-with-linux-jre build-zip-with-macosx-jre
+do
+  title "Build $target"
+  ant \
+    -Dnbplatform.active.dir="${NETBEANS_HOME}" \
+    -Dnbplatform.default.netbeans.dest.dir="${NETBEANS_HOME}" \
+    -Dnbplatform.default.harness.dir="${NETBEANS_HOME}"/harness \
+    -Dbuild.compiler.debug=true $target
+done
 
 #title "Build Portable Zip"
 #
